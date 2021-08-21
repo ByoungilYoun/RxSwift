3229ba4af58c9e82ba482fd5147eb4ce8480a2f0
YounByoungil <jimmy7525@naver.com>
2021-08-22 01:05:28 +0900

    Skip While operator

--- RxSwift_Tutorial/RxSwift_Tutorial/FilteringOperators.playground/Contents.swift
+++ RxSwift_Tutorial/RxSwift_Tutorial/FilteringOperators.playground/Contents.swift
@@ -44,9 +44,17 @@ let disposeBag = DisposeBag()
 
 // 4. skip operator
 
-Observable.of("a", "b", "c", "d", "e", "f")
-  .skip(3)
-  .subscribe(onNext: {
-    print($0) // 3개를 띄어넘고 d, e, f 가 프린트 된다.
-  }).disposed(by: disposeBag)
+//Observable.of("a", "b", "c", "d", "e", "f")
+//  .skip(3)
+//  .subscribe(onNext: {
+//    print($0) // 3개를 띄어넘고 d, e, f 가 프린트 된다.
+//  }).disposed(by: disposeBag)
 
+// 5. skip(while: )
+
+Observable.of(2,2,3,4,4)
+  .skip(while: {
+    $0 % 2 == 0 // 안의 조건을 만족할때까지 스킵을 하고 조건을 만족시키지 못하는 첫값이 나오면 그 뒤로 나머지 다 프린트 된다.
+  }).subscribe(onNext : {
+    print($0)
+  }).disposed(by: disposeBag)
